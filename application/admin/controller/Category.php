b0967958e6f199421d3dc9c0a2d5cb93847e5850
FerreWagner <18408229270@163.com>
2017-11-30 17:17:10 +0800

    list && add function success

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -4,6 +4,8 @@ namespace app\admin\controller;
 
 use app\admin\common\Base;
 use think\Request;
+use app\admin\model\Category as CateGoryModel;
+use think\Loader;
 
 class Category extends Base
 {
@@ -12,9 +14,33 @@ class Category extends Base
      *
      * @return \think\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        //cate data
+        $catemodel = new CateGoryModel();
+        $cate      = $catemodel->catetree();
+        //count cate data
+        $cate_count = CateGoryModel::count();
+        
+        //cate add
+        if($request->isPost()){
+            $_data = input('post.');
+            $validate = Loader::validate('Category');
+            if(!$validate->scene('add')->check($_data)){
+                $this->error($validate->getError());
+            }
+            
+//             $catemodel->data($_data);
+//             $_add = $catemodel->save();
+            $_add = $catemodel->create($_data);
+            if($_add){
+                $this->redirect('admin/category/index');
+            }else{
+                $this->error('Add Cate Error.Dear');
+            }
+        }
+        
+        $this->view->assign(['cate' => $cate, 'cate_count' => $cate_count]);
         return $this->view->fetch('category_list');
     }
 
@@ -37,6 +63,8 @@ class Category extends Base
     public function save(Request $request)
     {
         //
+        $_cate = new CateGoryModel();
+
     }
 
     /**
