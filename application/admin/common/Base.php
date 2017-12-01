96a10813dbb046d58cd4fd65c8bae2f7da510afc
FerreWagner <18408229270@163.com>
2017-12-01 17:35:26 +0800

    website config function success

--- application/admin/common/Base.php
+++ application/admin/common/Base.php
@@ -3,6 +3,7 @@ namespace app\admin\common;
 
 use think\Controller;
 use think\Request;
+use app\admin\model\System;
 
 class Base extends Controller
 {
@@ -14,6 +15,9 @@ class Base extends Controller
         if ($request->module() == 'admin' && $request->controller() != 'Login'){    //except Login action,all admin function need validate
             $this->isLogin();
         }
+        //查询网站的开关状态
+        $config = $this->getSystem();
+        $this->getStatus($request, $config);
     }
 
     protected function isLogin()
@@ -28,6 +32,28 @@ class Base extends Controller
     {
         if (!empty(\session('user_name'))){
             $this->error('You Already Login.Dear', 'admin/index/index');
+        }
+    }
+    
+    
+    
+    //获取配置信息
+    public function getSystem()
+    {
+        return System::get(1);
+    }
+    
+    //判断当前网站的开启状态($request请求对象,$config当前配置信息)
+    public function getStatus($request, $config)
+    {
+        //关闭前台，即不是admin模块
+        if ($request->module() != 'admin'){
+    
+            //根据配置表中的is_close进行判断，0开启1关闭
+            if ($config->is_close == 1){
+                $this->error('网站已关闭');
+                exit;
+            }
         }
     }
 }
--- application/admin/controller/System.php
+++ application/admin/controller/System.php
@@ -5,6 +5,7 @@ use app\admin\common\Base;
 use app\admin\model\Link;
 use think\Loader;
 use think\Request;
+use app\admin\model\System as SystemModel;
 class System extends Base
 {
     /**
@@ -15,27 +16,40 @@ class System extends Base
     public function index()
     {
         //
+        $this->view->assign('system', SystemModel::get(1));
+        
         return $this->view->fetch('system-set');
         
     }
+    
     /**
-     * 显示创建资源表单页.
-     *
-     * @return \think\Response
-     */
-    public function create()
-    {
-        //
-    }
-    /**
-     * 保存新建的资源
+     * 保存更新的资源
      *
      * @param  \think\Request  $request
      * @return \think\Response
      */
-    public function save(Request $request)
+    public function update(Request $request)
     {
-        //
+        if ($request->isAjax(true)){
+    
+            $data = $request->param();
+            //设置更新条件
+            $map = ['is_update' => $data['is_update']];
+            $res = SystemModel::update($data, $map);
+    
+            //返回信息
+            if (is_null($res)){
+                $status = 0;
+                $message = '更新失败，请检查';
+            }else {
+                $status = 1;
+                $message = '更新成功啦';
+            }
+    
+            return ['status' => $status, 'message' => $message];
+    
+        }
+    
     }
     /**
      * 显示 && 添加友链
@@ -104,35 +118,4 @@ class System extends Base
         $del ? $this->redirect('admin/system/linklist') : $this->error('Link Delete Error,Dear');
     }
     
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-    /**
-     * 删除指定资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function delete($id)
-    {
-        //
-    }
 }
