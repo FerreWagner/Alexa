80accdf6b9bed0d975a415d32ee816273b55be0b
FerreWagner <18408229270@163.com>
2017-12-07 17:30:33 +0800

    login data function success && member table success

--- application/index/common/Base.php
+++ application/index/common/Base.php
@@ -4,6 +4,7 @@ namespace app\index\common;
 use app\admin\model\System;
 use think\Controller;
 use think\Request;
+use think\Cookie;
 
 class Base extends Controller
 {
@@ -22,6 +23,12 @@ class Base extends Controller
                 'ip'   => $_SERVER['REMOTE_ADDR'],
                 'time' => time(),
             ]);
+        }
+        
+        if (Cookie::has('qq_accesstoken', 'alexa_') && Cookie::has('qq_openid', 'alexa_')){
+                $user_data = db('member')->where('openid', Cookie::get('qq_openid','alexa_'))->find();
+                if (!$user_data) $this->error('Your Data Have Not In DaaBase.');
+                $this->view->assign('alexau', $user_data);
         }
 
     }
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,6 +3,7 @@ namespace app\index\controller;
 
 use app\index\common\Base;
 use kuange\qqconnect\QC;
+use think\Cookie;
 
 class Index extends Base
 {
@@ -12,23 +13,45 @@ class Index extends Base
     }
     public function callback()
     {
+        error_reporting(E_ERROR | E_PARSE);
         //API调用示例：index.php->qqlogin.php->callback.php
         $qc = new QC();
 //        echo $_GET['code'];die;
         //以下为授权过程
         $access_token = $qc->qq_callback();    // access_token    //回调处理code,换取accesstoken和openID并保存到cookie中
         $openid =  $qc->get_openid();     // openid
-        //cookie时长一定要比accesstoken时长更短
-        setcookie('qq_accesstoken', $access_token, time() + 86400);
-        setcookie('qq_openid', $openid, time() + 86400);
-
-//        if (!isset($_COOKIE['qq_accesstoken']) || !isset($_COOKIE['qq_openid'])){
-//            return $this->error('Login Error,Dear');
-//        }
+        
+        
+        //cookie时长一定要比accesstoken过期时长更短
         // 待处理用户逻辑
-        $qc = new QC($access_token, $openid);
-        $qq_user_info = $qc->get_user_info();
-        dump($qq_user_info);die;
+        Cookie::init(['prefix'=>'alexa_','expire'=>86400,'path'=>'/']);
+        Cookie::set('qq_accesstoken', $access_token, 86400);
+        Cookie::set('qq_openid', $openid, 86400);
+        
+        if (Cookie::has('qq_accesstoken', 'alexa_') && Cookie::has('qq_openid', 'alexa_')){
+            @$openid_find = db('member')->where('openid', $openid)->find();
+            if (!$openid_find){
+                //第一次注册即入库
+                
+                $qc = new QC($access_token, $openid);
+                $qq_user_info = $qc->get_user_info();
+                //detail qq data
+                array_splice($qq_user_info, 0, 3);
+                $qq_user_info += [
+                    'time'   => time(), 
+                    'ip'     => $_SERVER['REMOTE_ADDR'], 
+                    'openid' => $openid
+                ];
+                $member_insert = db('member')->insert($qq_user_info);
+                $member_insert ? $this->redirect('index/index/index') : $this->error('Register Error.');
+            }else {
+                //已注册过
+                $this->redirect('index/index/index');
+            }
+            
+        }else {
+            die('Cookie Not Exists.');
+        }
 
 
 //        $this->success('登录成功', url('/'));
